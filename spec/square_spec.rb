181c042b13991b128a3bdf4d792c24969608ae57
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-02 13:58:07 -0700

    test: add tests for #add_occupant method

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -19,4 +19,21 @@ describe Square do
       end
     end
   end
+
+  describe '#add_occupant' do
+    context 'when the square is empty' do
+      subject(:empty_square) { described_class.new(name: 'name') }
+      xit 'adds the piece to the square' do
+        expect { empty_square.add_occupant(piece) }.to change { empty_square.occupant }.to(piece)
+      end
+    end
+
+    context 'when the square is already occupied' do
+      subject(:occupied_square) { described_class.new(name: 'name', occupant: piece) }
+      let(:piece_two) { double('piece two') }
+      xit 'add the piece to the square' do
+        expect { occupied_square.add_occupant(piece_two) }.to change { occupied_square.occupant }.to(piece_two)
+      end
+    end
+  end
 end
