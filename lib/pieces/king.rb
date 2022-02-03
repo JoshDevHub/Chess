53337da35769e6a35667607daaa79ba37e86bcfd
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-03 14:46:23 -0700

    chore: add boilerplate for King subclass of Piece

---
+++ lib/pieces/king.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../piece'
+
+# King subclass to model behavior of the King Chess piece
+class King < Piece
+  def initialize
+    super
+  end
+end
