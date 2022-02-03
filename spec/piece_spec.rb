37e4b94d7e5d4911def7db66a77cded203511ebe
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-03 14:33:47 -0700

    test: add first test case for Piece's piece factory method #self.create

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 describe Piece do
-  # placeholder
+  describe '#self.create' do
+    context 'when the piece is a rook' do
+      xit 'creates a Rook subclass' do
+        piece_name = 'R'
+        expect(rook_subclass).to receive(:new).with(:color)
+        described_class.create(piece_name)
+      end
+    end
+  end
 end
