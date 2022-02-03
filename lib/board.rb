dc565fe83c7c8e1b3342f723b6aef731756872fb
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-02 20:46:44 -0700

    feat: add #game_board, a structure to represent a chess playing board

---
+++ lib/board.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'square'
+
+# class to hold logic for the chess board
+class Board
+  attr_reader :game_board
+
+  def initialize(square)
+    @game_board = create_board(square)
+  end
+
+  HEIGHT = 8
+  WIDTH = 8
+
+  FILE_NAMES = %w[A B C D E F G H].freeze
+  RANK_NAMES = %w[1 2 3 4 5 6 7 8].freeze
+
+  private
+
+  def create_board(square)
+    Array.new(HEIGHT) do |rank|
+      Array.new(WIDTH) do |file|
+        square_name = FILE_NAMES[file] + RANK_NAMES[rank]
+        square.new(name: square_name)
+      end
+    end
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,18 +4,22 @@ require_relative '../lib/board'
 
 describe Board do
   describe '#game_board' do
-    subject(:board) { described_class.new }
     let(:square) { double('square') }
-    xit 'returns a 2d array with an outer size of 8' do
+    subject(:board) { described_class.new(square) }
+    before do
+      allow(square).to receive(:new)
+    end
+
+    it 'returns a 2d array with an outer size of 8' do
       expect(board.game_board.size).to eq(8)
     end
 
-    xit 'returns a 2d array with an inner size of 8' do
+    it 'returns a 2d array with an inner size of 8' do
       expect(board.game_board.all? { |inner| inner.size == 8 }).to be(true)
     end
 
-    xit 'returns a structure with 64 square objects' do
-      square_count = game_board.flatten.count(square)
+    it 'returns a structure with 64 objects' do
+      square_count = board.game_board.flatten.size
       expect(square_count).to eq(64)
     end
   end
