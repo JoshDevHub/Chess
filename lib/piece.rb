21bf793d8bd7d7d4235825c6a301bb754ba30a39
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-03 17:23:15 -0700

    feat: build factory method for instantiating subclasses

--- lib/piece.rb
+++ lib/piece.rb
@@ -2,7 +2,24 @@
 
 # Parent Piece class for game pieces to inherit from
 class Piece
-  def initialize
-    # placeholder
+  def initialize(color:)
+    @color = color
+  end
+
+  def self.create(piece:, color:)
+    case piece
+    when :king
+      King.new(color: color)
+    when :queen
+      Queen.new(color: color)
+    when :bishop
+      Bishop.new(color: color)
+    when :rook
+      Rook.new(color: color)
+    when :knight
+      Knight.new(color: color)
+    when :pawn
+      Pawn.new(color: color)
+    end
   end
 end
