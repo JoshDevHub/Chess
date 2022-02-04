9876b26eb6b49ab7b27091ce729fc989c6a689a1
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-04 09:10:26 -0700

    test: redesign factory tests

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
+require_relative '../lib/piece'
+Dir[File.join(__dir__, '../lib/pieces', '*.rb')].sort.each { |file| require file }
+
 describe Piece do
   describe '#self.create' do
-    context 'when the piece is a king' do
-      xit 'creates a King subclass' do
+    context 'when the piece is a white king' do
+      it 'creates a King subclass' do
         piece_name = :king
-        expect(rook_subclass).to receive(:new).with(:color)
-        described_class.create(piece_name)
+        color = 'white'
+        subclass = described_class.create(piece: piece_name, color: color)
+        expect(subclass).to be_a(King)
+      end
+    end
+
+    context 'when the piece is a black rook' do
+      it 'creates a Rook subclass' do
+        piece_name = :rook
+        color = 'black'
+        subclass = described_class.create(piece: piece_name, color: color)
+        expect(subclass).to be_a(Rook)
       end
     end
   end
