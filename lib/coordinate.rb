799a4d0b8023494b8d9d4232a49533f524879b97
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-04 22:42:59 -0700

    feat: add methods in coordinate module for translating notations
    `#to_square_notation` translates a set of x, y coordinates to the
    corresponding coordinate in square notation.
    
    `#to_coordinate` translates a square notation coordinate to a set of
    x, y coordinates

---
+++ lib/coordinate.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# module for handling coordinates in a 2d plane
+module Coordinate
+  FILE_NAMES = %w[A B C D E F G H].freeze
+  RANK_NAMES = %w[8 7 6 5 4 3 2 1].freeze
+
+  def to_square_notation(coordinate)
+    file_index, rank_index = coordinate
+    FILE_NAMES[file_index] + RANK_NAMES[rank_index]
+  end
+
+  def to_coordinate(square_notation)
+    x, y = square_notation.chars
+    [FILE_NAMES.index(x), RANK_NAMES.index(y)]
+  end
+end
