807ee125bb0afd2a7734cc0929507dcc0667ead0
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-07 10:43:25 -0700

    feat: add methods to respond to rook moveset

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -5,4 +5,20 @@ class Rook < Piece
   def self.handles_notation?(char)
     %w[R r].include?(char)
   end
+
+  def moves_horizontally?
+    true
+  end
+
+  def moves_up?
+    true
+  end
+
+  def moves_down?
+    true
+  end
+
+  def line_moves?
+    true
+  end
 end
