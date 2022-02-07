fec4cd9b2c701e42a6cfae3134aa44eedba6d3d3
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-07 08:52:19 -0700

    test: add tests for Bishop move methods

---
+++ spec/pieces/bishop_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/piece'
+require_relative '../../lib/pieces/bishop'
+
+describe Bishop do
+  subject(:bishop) { described_class.new(color: 'white') }
+  describe '#moves_diagonally?' do
+    it 'returns true' do
+      expect(bishop.moves_diagonally?).to be(true)
+    end
+  end
+
+  describe '#line_moves?' do
+    it 'returns true' do
+      expect(bishop.line_moves?).to be(false)
+    end
+  end
+end
