2097f2fdde493beabd372e0f92fea20949ee0465
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-11 14:20:29 -0700

    test: add cases for line move blocking

--- spec/moves/diagonal_moves_spec.rb
+++ spec/moves/diagonal_moves_spec.rb
@@ -54,7 +54,7 @@ describe DiagonalMoves do
     end
 
     context 'when the piece is a line piece' do
-      let(:piece) { double(line_moves?: true) }
+      let(:piece) { double(line_moves?: true, color: 'white') }
       let(:board) { double(square_empty?: true) }
       subject(:line_mover) { described_class.new(board: board, piece: piece) }
       context 'when no piece is blocking' do
@@ -70,6 +70,35 @@ describe DiagonalMoves do
           let(:square) { 'H1' }
           it 'returns a list containing G2, F3, E4, D5, C6, B7, and A8' do
             moves = %w[G2 F3 E4 D5 C6 B7 A8]
+            expect(line_mover.generate_moves(square)).to contain_exactly(*moves)
+          end
+        end
+      end
+
+      context 'when potential moves are blocked' do
+        let(:blocking_piece) { double(color: 'white') }
+        context 'when the origin square is B4 with a blocking piece on C5' do
+          let(:square) { 'B4' }
+          before do
+            allow(board).to receive(:square_empty?).with('C5').and_return(false)
+            allow(board).to receive(:piece_at).with('C5').and_return(blocking_piece)
+          end
+
+          it 'returns a list containing A5, A3, C3, D2, and E1' do
+            moves = %w[A5 A3 C3 D2 E1]
+            expect(line_mover.generate_moves(square)).to contain_exactly(*moves)
+          end
+        end
+
+        context 'when the origin square is H8 with a blocking piece on E5' do
+          let(:square) { 'H8' }
+          before do
+            allow(board).to receive(:square_empty?).with('E5').and_return(false)
+            allow(board).to receive(:piece_at).with('E5').and_return(blocking_piece)
+          end
+
+          it 'returns a list containing G7 and F6' do
+            moves = %w[G7 F6]
             expect(line_mover.generate_moves(square)).to contain_exactly(*moves)
           end
         end
