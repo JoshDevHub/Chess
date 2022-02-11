64c1a0a0805e2a498bf687cefa98423335fd687c
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-11 16:08:53 -0700

    feat: add MoveUp Move subclass and spec file

---
+++ lib/moves/move_up.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class MoveUp < Move
+  MOVESET = [
+    %i[up]
+  ].freeze
+end
---
+++ spec/moves/move_up_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/coordinate'
+require_relative '../../lib/move'
+require_relative '../../lib/moves/move_up'
+
+describe MoveUp do
+  describe '#generate_moves' do
+    context 'when the piece can only move one square' do
+      let(:piece) { double(line_moves?: false, color: 'white') }
+      context 'when no square is blocked' do
+        let(:board) { double(square_empty?: true) }
+        subject(:one_mover) { described_class.new(board: board, piece: piece) }
+        context 'when the starting square is G2' do
+          let(:square) { 'G2' }
+          it 'returns a list with G3' do
+            moves = %w[G3]
+            expect(one_mover.generate_moves(square)).to contain_exactly(*moves)
+          end
+        end
+
+        context 'when the starting square is H8' do
+          let(:square) { 'H8' }
+          it 'returns an empty array' do
+            expect(one_mover.generate_moves(square)).to be_empty
+          end
+        end
+      end
+    end
+  end
+end
