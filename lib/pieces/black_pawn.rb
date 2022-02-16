d46df05d439d91080635297a6714a38284ebfbef
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-16 13:39:38 -0700

    feat: implement double move for BlackPawn

--- lib/pieces/black_pawn.rb
+++ lib/pieces/black_pawn.rb
@@ -8,10 +8,42 @@ class BlackPawn < Piece
   end
 
   def moveset
-    [%i[down]]
+    :down
   end
 
   def self.handles_notation?(char)
     char == 'p'
+  end
+
+  def generate_moves(board)
+    if possible_double_move?
+      double_move(board)
+    else
+      single_move(board)
+    end
+  end
+
+  private
+
+  def double_move(board)
+    move_list = []
+    first_move = send(moveset, @position)
+    if legal_move?(first_move, board)
+      move_list << first_move
+      second_move = send(moveset, first_move)
+      move_list << second_move if legal_move?(second_move, board)
+    end
+    move_list
+  end
+
+  def single_move(board)
+    move_list = []
+    move = send(moveset, @position)
+    move_list << move if legal_move?(move, board)
+    move_list
+  end
+
+  def possible_double_move?
+    !@moved
   end
 end
--- spec/pieces/black_pawn_spec.rb
+++ spec/pieces/black_pawn_spec.rb
@@ -9,33 +9,68 @@ describe BlackPawn do
   let(:board) { instance_double(Board, square_empty?: true) }
   subject(:black_pawn) { described_class.new(color: 'black', position: square) }
   describe '#generate_moves' do
-    context 'when no friendly piece is blocking' do
-      context 'when the square is H2' do
-        let(:square) { 'H2' }
-        it 'returns a list containing H1' do
-          expect(black_pawn.generate_moves(board)).to contain_exactly('H1')
+    context 'when the piece has been moved' do
+      context 'when no friendly piece is blocking' do
+        context 'when the square is H2' do
+          let(:square) { 'H2' }
+          it 'returns a list containing H1' do
+            black_pawn.piece_moved
+            expect(black_pawn.generate_moves(board)).to contain_exactly('H1')
+          end
+        end
+
+        context 'when the square is G7' do
+          let(:square) { 'G7' }
+          it 'returns a list containing G6' do
+            black_pawn.piece_moved
+            expect(black_pawn.generate_moves(board)).to contain_exactly('G6')
+          end
         end
       end
 
-      context 'when the square is G7' do
-        let(:square) { 'G7' }
-        it 'returns a list containing G6' do
-          expect(black_pawn.generate_moves(board)).to contain_exactly('G6')
+      context 'when a friendly piece is blocking' do
+        let(:blocking_piece) { instance_double(Piece, color: 'black') }
+        context 'when the square is F6 and F5 is blocked' do
+          let(:square) { 'F6' }
+          before do
+            allow(board).to receive(:square_empty?).with('F5').and_return(false)
+            allow(board).to receive(:piece_at).with('F5').and_return(blocking_piece)
+          end
+
+          it 'returns an empty array' do
+            black_pawn.piece_moved
+            expect(black_pawn.generate_moves(board)).to be_empty
+          end
         end
       end
     end
 
-    context 'when a friendly piece is blocking' do
-      let(:blocking_piece) { instance_double(Piece, color: 'black') }
-      context 'when the square is F6 and F5 is blocked' do
-        let(:square) { 'F6' }
-        before do
-          allow(board).to receive(:square_empty?).with('F5').and_return(false)
-          allow(board).to receive(:piece_at).with('F5').and_return(blocking_piece)
+    context 'when the piece has not moved' do
+      context 'when no friendly piece is blocking' do
+        context 'when the square is B7' do
+          let(:square) { 'B7' }
+          it 'returns a list containing B6 and B5' do
+            moves = %w[B6 B5]
+            expect(black_pawn.generate_moves(board)).to contain_exactly(*moves)
+          end
+        end
+      end
+
+      context 'when squares are blocked' do
+        let(:blocking_piece) { instance_double(Piece, color: 'black') }
+        context 'when the square is A7 and A5 is blocked' do
+          let(:square) { 'A7' }
+          before do
+            allow(board).to receive(:square_empty?).with('A5').and_return(false)
+            allow(board).to receive(:piece_at).with('A5').and_return(blocking_piece)
+          end
+          it 'returns a list containing A6' do
+            expect(black_pawn.generate_moves(board)).to contain_exactly('A6')
+          end
         end
 
-        it 'returns an empty array' do
-          expect(black_pawn.generate_moves(board)).to be_empty
+        context 'when the square is F7 and F6 is blocked' do
+          let(:square) { 'F7' }
         end
       end
     end
