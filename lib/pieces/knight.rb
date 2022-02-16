ba1579efc6d3f0a448e0527897317721655a56f3
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-15 21:33:05 -0700

    refactor: move KnightMove functionality to Knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -2,18 +2,23 @@
 
 # Knight subclass to model behavior of the Knight chess piece
 class Knight < Piece
-  def initialize(color:)
+  def initialize(color:, position:)
     super
     @name = 'knight'
   end
 
+  MOVESET = [
+    %i[up up right],
+    %i[up up left],
+    %i[down down right],
+    %i[down down left],
+    %i[left left up],
+    %i[left left down],
+    %i[right right up],
+    %i[right right down]
+  ].freeze
+
   def self.handles_notation?(char)
     %w[N n].include?(char)
-  end
-
-  private
-
-  def knight_moves?
-    true
   end
 end
