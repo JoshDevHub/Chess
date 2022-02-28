6289ed722dbde725078f1283110515abe7e62172
jmsmith1018 <jmsmith1018@gmail.com>
2022-02-28 16:17:10 -0700

    feat: add promotion script and user input methods for pawn promotion

--- lib/chess.rb
+++ lib/chess.rb
@@ -21,9 +21,10 @@ class Chess
       display.print_board(@chess_board)
       break unless continue_game?
 
-      user_piece_position = user_piece_selection
-      user_move = user_move_selection(user_piece_position)
+      user_piece_position = user_origin_selection
+      user_move = user_target_selection(user_piece_position)
       @chess_board.move_piece(user_piece_position, user_move)
+      promotion_script(user_move)
       toggle_turns
     end
   end
@@ -34,15 +35,15 @@ class Chess
     @chess_board.self_check_filter(piece, initial_list)
   end
 
-  def user_piece_selection
+  def user_origin_selection
     loop do
       display.piece_choice(@active_player)
       input = user_square_input
-      return input if valid_piece_selection?(input)
+      return input if valid_origin_selection?(input)
     end
   end
 
-  def valid_piece_selection?(selection)
+  def valid_origin_selection?(selection)
     if !valid_square?(selection)
       display.input_error_message(:invalid_square)
     elsif @chess_board.square_empty?(selection)
@@ -54,7 +55,7 @@ class Chess
     end
   end
 
-  def user_move_selection(chosen_position)
+  def user_target_selection(chosen_position)
     move_list = create_move_list(chosen_position)
     loop do
       display.move_choice(move_list)
@@ -80,10 +81,39 @@ class Chess
     end
   end
 
+  def promotion_script(square)
+    return unless @chess_board.piece_at(square).can_promote?
+
+    user_selection = promotion_piece_selection
+    new_piece = Piece.from_fen(user_selection, square)
+    @chess_board.add_piece(new_piece, square)
+  end
+
+  def promotion_piece_selection
+    loop do
+      puts "#{@active_player}: your pawn has reached the back rank! It can promote to a new piece."
+      puts 'What piece do you wish to promote to? Enter a piece using its chess notation (Q, R, B, or N)'
+      selection = gets.chomp.upcase
+      return selection if valid_promotion_piece_selection?(selection)
+
+      display.input_error_message(:invalid_piece)
+    end
+  end
+
+  def valid_promotion_piece_selection?(selection)
+    %w[Q R B N].include?(selection)
+  end
+
   private
 
   def active_color
     @active_player.piece_color
+  end
+
+  def promote_piece(fen_symbol, position)
+    piece_fen = active_color == 'white' ? fen_symbol : fen_symbol.downcase
+    new_piece = Piece.from_fen(piece_fen, position)
+    @chess_board.add_piece(new_piece, position)
   end
 
   def user_square_input
