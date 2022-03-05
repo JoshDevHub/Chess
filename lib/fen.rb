ed6fbf31437d3766d2734662abe9edfa8300c995
jmsmith1018 <jmsmith1018@gmail.com>
2022-03-05 10:56:44 -0700

    chore: cleanup #piece_info method

--- lib/fen.rb
+++ lib/fen.rb
@@ -13,14 +13,10 @@ class FEN
 
   def piece_info
     info = fen_string.split(' ')[0].chars.map(&empty_expand).join('').split('/')
-    pieces = []
-    info.each_with_index do |rank, y_coord|
-      rank.each_char.with_index do |chr, x_coord|
-        square_name = to_square_notation([x_coord, y_coord])
-        pieces << @piece.from_fen(chr, square_name) unless chr == '.'
-      end
+    info.each_with_object([]).with_index do |(rank, pieces), y_coord|
+      row_info = parse_ranks(rank, y_coord)
+      pieces.concat(row_info)
     end
-    pieces
   end
 
   def active_color
@@ -33,8 +29,8 @@ class FEN
   end
 
   def en_passant_target
-    en_passant_square = fen_string.split(' ')[3]
-    en_passant_square == '-' ? nil : en_passant_square.upcase
+    en_passant_square = fen_string.split(' ')[3].upcase
+    en_passant_square unless en_passant_square == '-'
   end
 
   def half_move_clock
@@ -48,6 +44,15 @@ class FEN
   end
 
   private
+
+  def parse_ranks(rank, y_coord)
+    pieces = []
+    rank.each_char.with_index do |chr, x_coord|
+      square_name = to_square_notation([x_coord, y_coord])
+      pieces << @piece.from_fen(chr, square_name) unless chr == '.'
+    end
+    pieces
+  end
 
   def empty_expand
     lambda do |character|
