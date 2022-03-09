5bda125f1e1a0a69c60bec1452dfd5fbc8640b2f
jmsmith1018 <jmsmith1018@gmail.com>
2022-03-09 13:52:14 -0700

    feat: add #valid_origin? API to analyze user input on a piece to move

--- lib/move_interface.rb
+++ lib/move_interface.rb
@@ -26,4 +26,41 @@ class MoveInterface
   def move_selection
     nil
   end
+
+  def valid_origin?(origin)
+    return false unless valid_square_name?(origin)
+
+    square = @board.access_square(origin)
+    occupied_square?(square) && piece_color_matches_player?(square) &&
+      origin_contains_legal_moves?(square)
+  end
+
+  private
+
+  def valid_square_name?(square_name)
+    return true if valid_square?(square_name)
+
+    @display.input_error_message(:invalid_square)
+  end
+
+  def occupied_square?(square)
+    return true unless square.unoccupied?
+
+    @display.input_error_message(:empty_square)
+  end
+
+  def piece_color_matches_player?(square)
+    return true if square.piece_color == @active_player.piece_color
+
+    @display.input_error_message(:wrong_color)
+  end
+
+  def origin_contains_legal_moves?(square)
+    piece = square.piece
+    list = piece.move_list(@board)
+    legal_move_list = @board.self_check_filter(piece, list)
+    return true unless legal_move_list.empty?
+
+    @display.input_error_message(:no_moves)
+  end
 end
--- spec/move_interface_spec.rb
+++ spec/move_interface_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require_relative '../lib/coordinate'
+require_relative '../lib/square'
 require_relative '../lib/board'
+require_relative '../lib/piece'
 require_relative '../lib/display'
 require_relative '../lib/player'
 require_relative '../lib/move_interface'
@@ -10,7 +12,7 @@ require_relative '../lib/move_interfaces/move_inline_interface'
 
 describe MoveInterface do
   let(:board) { instance_double(Board) }
-  let(:player) { instance_double(Player) }
+  let(:player) { instance_double(Player, piece_color: 'white') }
   let(:display) { instance_double(Display) }
   describe '#self.for_input' do
     subject(:move_interface) { described_class }
@@ -87,6 +89,113 @@ describe MoveInterface do
     end
     it 'returns nil' do
       expect(default_move_interface.move_selection).to be(nil)
+    end
+  end
+
+  describe '#valid_origin' do
+    subject(:move_interface) { described_class.new(**arguments) }
+    let(:arguments) do
+      {
+        board: board,
+        active_player: player,
+        display: display,
+        user_input: 'E2E4'
+      }
+    end
+    context 'when the origin is valid' do
+      let(:color) { 'white' }
+      let(:e2_square) { instance_double(Square, piece_color: color, unoccupied?: false) }
+      let(:piece) { instance_double(Piece, move_list: move_list) }
+      let(:move_list) { %w[E3 E4] }
+      before do
+        allow(board).to receive(:access_square).with('E2').and_return(e2_square)
+        allow(board).to receive(:self_check_filter).with(piece, move_list).and_return(move_list)
+        allow(player).to receive(:piece_color).and_return(color)
+        allow(display).to receive(:input_error_message)
+        allow(e2_square).to receive(:piece).and_return(piece)
+      end
+
+      it 'returns true' do
+        origin = 'E2'
+        expect(move_interface.valid_origin?(origin)).to be(true)
+      end
+    end
+
+    context 'when the origin is invalid' do
+      context 'when the square does not exist in the game' do
+        let(:origin) { 'F@' }
+        before do
+          allow(display).to receive(:input_error_message)
+        end
+        it 'returns falsey' do
+          expect(move_interface.valid_origin?(origin)).to be_falsey
+        end
+
+        it 'sends display #input_error_message with :invalid_square' do
+          expect(display).to receive(:input_error_message).with(:invalid_square)
+          move_interface.valid_origin?(origin)
+        end
+      end
+
+      context 'when the origin square is empty' do
+        let(:origin) { 'E2' }
+        let(:e2_square) { instance_double(Square) }
+        before do
+          allow(display).to receive(:input_error_message)
+          allow(board).to receive(:access_square).with(origin).and_return(e2_square)
+          allow(e2_square).to receive(:unoccupied?).and_return(true)
+        end
+        it 'returns falsey' do
+          expect(move_interface.valid_origin?(origin)).to be_falsey
+        end
+
+        it 'sends display #input_error_message with :empty_square' do
+          expect(display).to receive(:input_error_message).with(:empty_square)
+          move_interface.valid_origin?(origin)
+        end
+      end
+
+      context 'when the piece color of the origin square does not match player color' do
+        let(:origin) { 'E2' }
+        let(:opponent_color) { 'black' }
+        let(:e2_square) { instance_double(Square, unoccupied?: false) }
+        before do
+          allow(display).to receive(:input_error_message)
+          allow(board).to receive(:access_square).with(origin).and_return(e2_square)
+          allow(e2_square).to receive(:piece_color).and_return(opponent_color)
+        end
+        it 'returns falsey' do
+          expect(move_interface.valid_origin?(origin)).to be_falsey
+        end
+
+        it 'sends display #input_error_message with :wrong_color' do
+          expect(display).to receive(:input_error_message).with(:wrong_color)
+          move_interface.valid_origin?(origin)
+        end
+      end
+
+      context 'when the piece has no legal moves' do
+        let(:origin) { 'E2' }
+        let(:e2_square) { instance_double(Square, unoccupied?: false, piece_color: 'white') }
+        let(:piece) { instance_double(Piece) }
+        let(:move_list) { [] }
+        before do
+          allow(display).to receive(:input_error_message)
+          allow(board).to receive(:access_square).with(origin).and_return(e2_square)
+          allow(e2_square).to receive(:piece).and_return(piece)
+          allow(piece).to receive(:move_list).and_return(move_list)
+          allow(board).to receive(:self_check_filter).with(piece, move_list).and_return(move_list)
+        end
+
+        it 'returns falsey' do
+          expect(move_interface.valid_origin?(origin)).to be_falsey
+        end
+
+        it 'sends display #input_error_message with :no_moves' do
+          expect(display).to receive(:input_error_message).with(:no_moves)
+          move_interface.valid_origin?(origin)
+        end
+      end
     end
   end
 end
