44d07f13fb26d6fd77b2659d664b897a9181071f
jmsmith1018 <jmsmith1018@gmail.com>
2022-03-10 14:08:59 -0700

    feat: build MoveListInterface
    This class gives users an interface where a square can be entered and then
    a list of possible moves from the chosen square are displayed. This is
    meant to be a more beginner friendly interface for users less familiar
    with how pieces move in Chess.

--- lib/move_interfaces/move_list_interface.rb
+++ lib/move_interfaces/move_list_interface.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
+# class for giving the user a UI that shows possible moves from a square input
 class MoveListInterface < MoveInterface
+  def move_selection
+    return unless valid_origin?(user_input)
+
+    move_list = board.move_list_from_origin(user_input, castle_manager)
+    target = user_target_input(move_list)
+    return unless target
+
+    { origin: user_input, target: target }
+  end
+
+  private
+
+  def user_target_input(move_list)
+    loop do
+      display.move_choice_prompt(move_list)
+      target_input = gets.upcase.gsub(/[[:space:]]/, '')
+      return target_input if valid_target?(target_input, move_list)
+    end
+  end
 end
---
+++ spec/move_interfaces/move_list_interface_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/coordinate'
+require_relative '../../lib/board'
+require_relative '../../lib/display'
+require_relative '../../lib/move_interface'
+require_relative '../../lib/move_interfaces/move_list_interface'
+
+describe MoveListInterface do
+  describe '#move_selection' do
+    let(:board) { instance_double(Board) }
+    let(:active_color) { 'white' }
+    let(:display) { double(Display) }
+    let(:castle_manager) { double('castle') }
+    let(:interface_arguments) do
+      {
+        board: board,
+        active_color: active_color,
+        display: display,
+        castle_manager: castle_manager,
+        user_input: user_input
+      }
+    end
+    subject(:move_list_interface) { described_class.new(**interface_arguments) }
+    context 'when the user input is invalid' do
+      let(:user_input) { 'E2' }
+      before do
+        allow(move_list_interface).to receive(:valid_origin?).with(user_input).and_return(false)
+      end
+      it 'returns nil' do
+        expect(move_list_interface.move_selection).to be(nil)
+      end
+    end
+
+    context 'when the target input is valid immediately' do
+      let(:user_input) { 'E2' }
+      let(:user_target) { 'E4' }
+      let(:target_list) { %w[E3 E4] }
+      before do
+        allow(move_list_interface).to receive(:valid_origin?).with(user_input).and_return(true)
+        allow(board).to receive(:move_list_from_origin).and_return(target_list)
+        allow(display).to receive(:move_choice_prompt)
+        allow(display).to receive(:input_error_message)
+        allow(move_list_interface).to receive(:gets).and_return('E4')
+      end
+      it 'sends #move_list_from_origin to board with user_input and castle_manager' do
+        expect(board).to receive(:move_list_from_origin).with(user_input, castle_manager)
+        move_list_interface.move_selection
+      end
+
+      it 'sends #move_choice_prompt to display' do
+        expect(display).to receive(:move_choice_prompt)
+        move_list_interface.move_selection
+      end
+
+      it 'returns a hash with the key: origin set to user_input' do
+        returned_hash = move_list_interface.move_selection
+        expect(returned_hash[:origin]).to eq(user_input)
+      end
+
+      it 'returns a hash with the key: target set to user_target' do
+        returned_hash = move_list_interface.move_selection
+        expect(returned_hash[:target]).to eq(user_target)
+      end
+    end
+
+    context 'when the target input is invalid once and then valid' do
+      let(:user_input) { 'E2' }
+      let(:user_target) { 'E4' }
+      let(:target_list) { %w[E3 E4] }
+      let(:bad_input) { 'D4' }
+      before do
+        allow(move_list_interface).to receive(:valid_origin?).with(user_input).and_return(true)
+        allow(board).to receive(:move_list_from_origin).and_return(target_list)
+        allow(display).to receive(:move_choice_prompt)
+        allow(display).to receive(:input_error_message)
+        allow(move_list_interface).to receive(:gets).and_return('D4', 'E4')
+      end
+      it 'sends #move_list_from_origin to board with user_input and castle_manager' do
+        expect(board).to receive(:move_list_from_origin).with(user_input, castle_manager)
+        move_list_interface.move_selection
+      end
+
+      it 'sends #move_choice_prompt to display twice' do
+        expect(display).to receive(:move_choice_prompt).twice
+        move_list_interface.move_selection
+      end
+
+      it 'returns a hash with the key: origin set to user_input' do
+        returned_hash = move_list_interface.move_selection
+        expect(returned_hash[:origin]).to eq(user_input)
+      end
+
+      it 'returns a hash with the key: target set to user_target' do
+        returned_hash = move_list_interface.move_selection
+        expect(returned_hash[:target]).to eq(user_target)
+      end
+    end
+  end
+end
