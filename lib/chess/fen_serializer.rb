93edf9977f06897d69df70572572fe5bbdd907ba
jmsmith1018 <jmsmith1018@gmail.com>
2022-03-17 12:23:43 -0600

    feat: build out methods for loading game and scripting saves/loads

--- lib/chess/fen_serializer.rb
+++ lib/chess/fen_serializer.rb
@@ -28,12 +28,48 @@ class Chess
     def save_game
       yaml_string = game_to_yaml
       Dir.mkdir('../saved_games') unless Dir.exist?('../saved_games')
-      directory_size = Dir.entries('../saved_games').size
-      game_number = directory_size >= 5 ? 1 : directory_size
+      save_numbers = Dir.each_child('saved_games/').to_a.map(&:chr)
+      game_number = ('1'..'5').find { |number| save_numbers.none?(number) } || '1'
+      manage_save_files(game_number)
       white_player = instance_variable_get(:@player_white)
       black_player = instance_variable_get(:@player_black)
       filename = "saved_games/#{game_number}_#{white_player}_vs_#{black_player}.yaml"
       File.open(filename, 'w') { |file| file.puts yaml_string }
+    end
+
+    def manage_save_files(number)
+      duplicate_dir = Dir.entries('saved_games/').find { |file| file.include?(number) }
+      delete_save("saved_games/#{duplicate_dir}") if duplicate_dir
+    end
+
+    def delete_save(file_path)
+      File.delete(file_path) if File.exist?(file_path)
+    end
+
+    def load_script
+      load_file = load_game_selection
+      file_path = "saved_games/#{load_file}"
+      loaded_game = load_from_yaml(file_path)
+      delete_save(file_path)
+      loaded_game.game_script
+    end
+
+    def load_game_selection
+      save_list = Dir.each_child('saved_games/').to_a
+      return display.no_saves_error if save_list.empty?
+
+      display.load_game_prompt(save_list)
+      user_save_selection = choose_save(save_list)
+      save_list.find { |save| save[0] == user_save_selection }
+    end
+
+    def choose_save(save_list)
+      loop do
+        user_input = gets.chomp
+        return user_input if save_list.any? { |save| save[0] == user_input }
+
+        display.input_error_message(:save_number)
+      end
     end
 
     def load_from_yaml(file_path)
