407ca1a136eceaad0a313777de9b85266eb3a458
jmsmith1018 <jmsmith1018@gmail.com>
2022-03-17 10:49:41 -0600

    refactor: changes to control the size of /saved_games directory

--- lib/chess/fen_serializer.rb
+++ lib/chess/fen_serializer.rb
@@ -9,7 +9,7 @@ class Chess
     def game_to_fen
       en_passant_square = instance_variable_get(:@chess_board).en_passant_target || '-'
       board_fen = instance_variable_get(:@chess_board).to_fen
-      active_color = instance_variable_get(:@active_player).piece_color.chr
+      active_color = instance_variable_get(:@active_color).chr
       castle_fen = instance_variable_get(:@castle_manager).to_fen
       half_move_fen = instance_variable_get(:@half_move_clock)
       full_move_fen = instance_variable_get(:@full_move_clock)
@@ -27,11 +27,13 @@ class Chess
 
     def save_game
       yaml_string = game_to_yaml
-      Dir.mkdir('saved_games') unless Dir.exist?('saved_games')
-      filename = 'saved_games/saved_chess.yaml'
-      File.open(filename, 'w') do |file|
-        file.puts yaml_string
-      end
+      Dir.mkdir('../saved_games') unless Dir.exist?('../saved_games')
+      directory_size = Dir.entries('../saved_games').size
+      game_number = directory_size >= 5 ? 1 : directory_size
+      white_player = instance_variable_get(:@player_white)
+      black_player = instance_variable_get(:@player_black)
+      filename = "saved_games/#{game_number}_#{white_player}_vs_#{black_player}.yaml"
+      File.open(filename, 'w') { |file| file.puts yaml_string }
     end
   end
 end
