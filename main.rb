8f0ac2f03f9dd47b560d5e35594fc8c14649845a
jmsmith1018 <jmsmith1018@gmail.com>
2022-03-17 12:33:48 -0600

    chore: change method call on Chess to start the game with new intro

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'lib/coordinate'
+require_relative 'lib/chess/fen_serializer'
 Dir[File.join(__dir__, '/lib/**', '*.rb')].sort.each { |file| require file }
 
-Chess.new.play_chess
+Chess.new.play
